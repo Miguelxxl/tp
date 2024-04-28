6cb8a5a7a66ceb63927edcceb43b9a706c0a6834
Miguelxxl <162146231+Miguelxxl@users.noreply.github.com>
2024-04-28 19:35:17 +0100

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Brought To You By Fredy Mouaffo</b>
+    </div>
+    <strong>Copyright &copy; 2024 Library Attendance System </strong>
+</footer>
